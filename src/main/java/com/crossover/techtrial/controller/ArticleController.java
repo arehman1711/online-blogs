5686231c18c811d63265812aa1c7b5aa6025a542
Abdur Rehman <a.rehman1711@gmail.com>
2023-01-10 23:56:36 +0530

    Add thymleaf support

--- src/main/java/com/crossover/techtrial/controller/ArticleController.java
+++ src/main/java/com/crossover/techtrial/controller/ArticleController.java
@@ -5,23 +5,22 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
 
 import com.crossover.techtrial.model.Article;
 import com.crossover.techtrial.service.ArticleService;
 
-@RestController
+@Controller
 public class ArticleController {
 
 	@Autowired
 	private ArticleService articleService;
+
+	@RequestMapping(path = "/")
+	public String index () {
+		return "index";
+	}
 
 	@PostMapping(path = "articles")
 	public ResponseEntity<Article> createArticle(@RequestBody Article article) {
